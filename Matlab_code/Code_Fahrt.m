3cc039aac777198bf178b96b3129ed61dedf4c76
fynnbuchsteiner-1 <bf3044@mci4me.at>
2025-07-30 17:19:11 +0200

    Add files via upload

--- Matlab_code/Code_Fahrt.m
+++ Matlab_code/Code_Fahrt.m
@@ -2,13 +2,14 @@
 % --- Parameterdefinition ---
 roh = 1025;                          % Dichte Wasser [kg/m^3]
 A = 0.059;                           % Fläche Frontwing [m^2]
-Cl = 1.02;                           % Auftriebsbeiwert
-Cd = 0.25;                           % Widerstandsbeiwert
+Cl = 0.64;                           % Auftriebsbeiwert
+Cd = 0.048;                          % Widerstandsbeiwert
 X_cop = 0.151;                       % Center of Pressure [m]
 X_v = 0.0725;                        % Abstand Punkt A zum Mast [m]
 X_h = 0.095;                         % Abstand Punkt B zum Mast [m]
-X_ml_vec = [0.6, 0.75, 0.9, 1.1];  % Mastlängen [m]
+X_ml_vec = [0.6, 0.75, 0.9, 1.1];    % Mastlängen [m]
 v_vec = 0:0.5:18;                    % Geschwindigkeitsbereich [m/s]
+F_threshold = -360;                 % Auslösekraft [N]
 
 linestyles = {'-', '--', ':', '-.', ':'};  % 5 unterscheidbare Linienarten
 
@@ -33,21 +34,48 @@ end
 % --- Plot: FAY ---
 figure;
 subplot(2,1,1); hold on;
+
+% Bereichsfärbung mit Handles für Legende
+y_max = max(FAY_all, [], 'all') + 100;
+y_min = min(FAY_all, [], 'all') - 100;
+ylim([y_min, y_max]);
+
+% Grün oberhalb der Auslösegrenze (Funktionsbereich)
+h_green = area(v_vec, y_max * ones(size(v_vec)), F_threshold, ...
+    'FaceColor', [0.85 1 0.85], 'EdgeColor', 'none', ...
+    'ShowBaseLine', 'off', 'DisplayName', 'Funktionsbereich');
+
+% Linienplots
 for i = 1:length(X_ml_vec)
     plot(v_vec, FAY_all(i,:), linestyles{i}, 'LineWidth', 1.5, ...
         'DisplayName', sprintf('X_{ml} = %.2f m', X_ml_vec(i)));
 end
+
+
 xlabel('Geschwindigkeit /ms^{-1}');
 ylabel('F_{ay} /N');
-legend('Location', 'northwest'); grid on;
+legend('Location', 'northwest');
+grid on;
+%title('F_{ay} bei verschiedenen Mastlängen');
 
 % --- Plot: FBY ---
 subplot(2,1,2); hold on;
+
+% Bereichsfärbung komplett grün
+y_max_fby = max(FBY_all, [], 'all') + 50;
+y_min_fby = min(FBY_all, [], 'all') - 50;
+ylim([y_min_fby, y_max_fby]);
+h_green_fby = area(v_vec, y_max_fby * ones(size(v_vec)), y_min_fby, ...
+    'FaceColor', [0.85 1 0.85], 'EdgeColor', 'none', ...
+    'ShowBaseLine', 'off', 'DisplayName', 'Funktionsbereich');
+
+% Linienplots
 for i = 1:length(X_ml_vec)
     plot(v_vec, FBY_all(i,:), linestyles{i}, 'LineWidth', 1.5, ...
         'DisplayName', sprintf('X_{ml} = %.2f m', X_ml_vec(i)));
 end
+
 xlabel('Geschwindigkeit /ms^{-1}');
 ylabel('F_{by} /N');
 legend('Location', 'northwest'); grid on;
-
+%title('F_{by} bei verschiedenen Mastlängen');
--- Matlab_code/Code_Jump.m
+++ Matlab_code/Code_Jump.m
@@ -1,13 +1,13 @@
 % =================== Lastfall Sprung ===================
 % --- Parameterdefinition ---
-roh = 1025;                          % Dichte Wasser [kg/m^3]
-A = 0.059;                           % Fläche Frontwing [m^2]
-X_cop = 0.151;                       % Center of Pressure [m]
-X_v = 0.0725;                        % Abstand Punkt A zum Mast [m]
-X_h = 0.095;                         % Abstand Punkt B zum Mast [m]
-X_ml = 0.8;                          % Fixierte Mastlänge für Slamming-Analyse [m]
-v_vec = 0:0.5:10;                    % Geschwindigkeitsbereich [m/s]
-C_vec = [1.5, 2.0, 2.5, 3.0];        % Slamming-Koeffizienten
+roh = 1025;                       % Dichte Wasser [kg/m^3]
+A = 0.059;                        % Fläche Frontwing [m^2]
+X_cop = 0.151;                    % Center of Pressure [m]
+X_v = 0.0725;                     % Abstand Punkt A zum Mast [m]
+X_h = 0.095;                      % Abstand Punkt B zum Mast [m]
+X_ml = 0.8;                       % Fixierte Mastlänge [m]
+v_vec = 0:0.5:10;                 % Geschwindigkeitsbereich [m/s]
+C_vec = [1.5, 2.0, 2.5, 3.0];     % Slamming-Koeffizienten
 linestyles = {'-', '--', ':', '-.'};
 
 % --- Initialisierung ---
@@ -27,16 +27,31 @@ for i = 1:length(C_vec)
     end
 end
 
-% --- Plot: FAY ---
+% --- Plot: FAY mit grünem Hintergrund ---
 figure;
 subplot(2,1,1); hold on;
+
+% Hintergrundfläche für Funktionsbereich
+FAY_min = min(FAY_all(:)) * 0.9;
+FAY_max = max(FAY_all(:)) * 1.1;
+x_fill = [v_vec, fliplr(v_vec)];
+y_fill = [FAY_max * ones(1, length(v_vec)), FAY_min * ones(1, length(v_vec))];
+fill(x_fill, y_fill, [0.85 1 0.85], 'EdgeColor', 'none', ...
+     'DisplayName', 'Funktionsbereich');
+
+% FAY-Kurven
 for i = 1:length(C_vec)
     plot(v_vec, FAY_all(i,:), linestyles{i}, 'LineWidth', 1.5, ...
         'DisplayName', sprintf('C = %.1f', C_vec(i)));
 end
+
 xlabel('Geschwindigkeit /ms^{-1}');
 ylabel('F_{ay} /N');
-legend('Location', 'northwest'); grid on;
+legend('Location', 'northwest');
+ylim([FAY_min, FAY_max]);
+xlim([0 10]);
+%title('F_{ay} bei unterschiedlichen Slamming-Koeffizienten');
+grid on;
 
 % --- Plot: FBY mit Scharniergrenze ---
 subplot(2,1,2); hold on;
@@ -72,4 +87,5 @@ ylabel('F_{by} /N');
 legend('Location', 'best');
 xlim([0 10]);
 ylim([FBY_min, FBY_max]);
+%title('F_{by} mit Scharniergrenze bei unterschiedlichen Slamming-Koeffizienten');
 xticks(0:1:10); grid on;
